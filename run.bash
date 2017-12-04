57b43341caab03860e8b6443d78a06f89c381757
Valentin Dzhankhotov <valentin.dzhankhotov@gef.fi>
2017-12-04 17:41:47 +0200

    install ruby-dev

--- run.bash
+++ run.bash
@@ -1,5 +1,7 @@
+# sudo apt install ruby-dev
 # sudo apt install gem
-# sudo gem install jekyll bundler
+# sudo gem install jekyll
+# sudo gem install bundler
 # bundle install
 
 bundle exec jekyll serve --watch
