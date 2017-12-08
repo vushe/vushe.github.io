52f23dd68b896bad59f814dff7707d919f78790a
Valentin Dzhankhotov <valentin.dzhankhotov@gef.fi>
2017-12-08 12:11:07 +0200

    livereload server added

--- run.bash
+++ run.bash
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 # sudo apt install ruby-dev
 # sudo apt install gem
 # sudo gem install jekyll
 # sudo gem install bundler
 # bundle install
 
+gnome-terminal --window -e livereload
 bundle exec jekyll serve --watch
